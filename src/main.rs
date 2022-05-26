9d86e92adbfd4d563bfc52c395156189293bc298
lolicon <lolicon@mail.az>
2022-05-26 10:32:55 +0800

    feat: formating format

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-mod format_display_list;
-pub mod format_print;
-mod format_display;
-mod primitives_tuples;
 mod custom_type_structures;
+mod format_display;
+mod format_display_list;
+mod format_formating;
+mod format_print;
+mod primitives_tuples;
 
 fn main() {
     println!("hello world");
